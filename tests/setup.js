d47f80ddca07fc26e35ff39a72f3abbf79726d29
J. Jenkins <jeremiah.jenkins@gmail.com>
2025-09-03 12:47:27 -0700

    Updated setup.js

--- tests/setup.js
+++ tests/setup.js
@@ -2,9 +2,12 @@
 process.env.NODE_ENV = 'test';
 process.env.RECHARGE_API_KEY = 'test_api_key_12345';
 process.env.RECHARGE_API_URL = 'https://api.rechargeapps.com';
-process.env.RECHARGE_API_TIMEOUT = '5000';
-process.env.RECHARGE_API_RETRY_ATTEMPTS = '1';
+process.env.RECHARGE_API_TIMEOUT = '10000';
+process.env.RECHARGE_API_RETRY_ATTEMPTS = '2';
 process.env.RECHARGE_API_RETRY_DELAY = '100';
+
+// Increase timeout for integration tests
+jest.setTimeout(60000);
 
 // Mock console methods to reduce test noise
 global.console = {
@@ -12,4 +15,4 @@ global.console = {
   error: jest.fn(),
   warn: jest.fn(),
   log: jest.fn()
-};
+}
